16e0e097da8c90e4e8e14bbf6b0166edc9eb7e1e
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-01-31 15:02:21 -0500

    create Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-import Rating from '../Rating'
+import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
