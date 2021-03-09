a49d1e5e98eda37926c8571db33f3835d997bfc5
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-03-09 12:41:19 -0500

    custome error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 
@@ -14,6 +15,10 @@ app.get('/', (req, res) => {
   res.send('API is now running...')
 })
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(
