81a13e2929a3fe961fcd25bfef5a8c19cbd93b5c
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-04-15 17:00:28 -0400

    User Registration and Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,14 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    //part of mongoose that allows us to see whether a field was modified
+    next()
+  }
+  const salt = await bcrypt.genSalt(10) //salt will hash the password asynchronously, bcrypt returns a promise so we must have await
+  this.password = await bcrypt.hash(this.password, salt) //password pertains to the user being created, his password, it takes in the salt as a second argument
+})
 const User = mongoose.model('User', userSchema)
 
 export default User
