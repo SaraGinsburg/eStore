b5b36852d57d488808260b72f8b64cffc96a04a7
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-04-29 13:47:04 -0400

    Order Details Reducer & Action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -3,6 +3,9 @@ import {
   ORDER_CREATE_SUCCESS,
   ORDER_CREATE_FAIL,
   ORDER_CREATE_RESET,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -12,6 +15,24 @@ export const orderCreateReducer = (state = {}, action) => {
     case ORDER_CREATE_SUCCESS:
       return { loading: false, success: true, order: action.payload }
     case ORDER_CREATE_FAIL:
+      return { loading: false, error: action.payload }
+    case ORDER_CREATE_RESET:
+      return {}
+    default:
+      return state
+  }
+}
+
+export const orderDetailsReducer = (
+  state = { orderItems: [], shippingAddres: {} },
+  action
+) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return { ...state, loading: true }
+    case ORDER_DETAILS_SUCCESS:
+      return { loading: false, order: action.payload }
+    case ORDER_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case ORDER_CREATE_RESET:
       return {}
