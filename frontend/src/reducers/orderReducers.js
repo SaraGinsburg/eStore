9875afa33523e48b3051742dc9b346d083016a09
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-05-02 01:52:44 -0400

    Order Screen

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -24,7 +24,7 @@ export const orderCreateReducer = (state = {}, action) => {
 }
 
 export const orderDetailsReducer = (
-  state = { orderItems: [], shippingAddres: {} },
+  state = { loading: true, orderItems: [], shippingAddres: {} },
   action
 ) => {
   switch (action.type) {
