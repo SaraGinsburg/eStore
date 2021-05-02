a175dfd7ccdf3f353a49c660d200c58c6d1374aa
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2021-05-02 02:05:46 -0400

    Update To Paid Endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -51,4 +51,26 @@ const getOrderById = asyncHandler(async (req, res) => {
     throw new Eror('Order not Found')
   }
 })
-export { addOrderItems, getOrderById }
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  if (order) {
+    order.isPaid = true
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    }
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Eror('Order not Found')
+  }
+})
+export { addOrderItems, getOrderById, updateOrderToPaid }
