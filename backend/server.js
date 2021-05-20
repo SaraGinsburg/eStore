6401f25ff33c2cd76d95aee8912155571642041c
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2021-05-19 21:58:15 -0400

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from 'path' //path is a node.js module to work with file paths
 import express from 'express'
 
 import dotenv from 'dotenv'
@@ -8,6 +8,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -21,11 +22,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads'))) //path.join() so we can join some segments of files, __dirname will give the current folder
 app.use(notFound)
 app.use(errorHandler)
 
