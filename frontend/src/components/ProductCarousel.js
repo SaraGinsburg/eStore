c23bc42ace0e9996b2437aeaba2dfad98619470a
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2021-06-17 11:54:53 -0400

    minor typos

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -26,7 +26,7 @@ const ProductCarousel = () => {
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption classname="carousel-caption">
+            <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name}(${product.price})
               </h2>
